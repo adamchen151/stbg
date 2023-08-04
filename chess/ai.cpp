c8747ec9a85465f741ae10ee62d39fd7d6003d02
adamchen151 <105564290+adamchen151@users.noreply.github.com>
2023-08-04 16:27:07 -0400

    Update ai.cpp

--- chess/ai.cpp
+++ chess/ai.cpp
@@ -1,45 +1,45 @@
+#include "functions.h"
+#include "move.h"
 #include <iostream>
 #include <vector>
-#include <stdlib.h>
-#include "functions.h"
+// TO-DO: give depth 2
 
-// AI only white for now
-int get_piece_value(int i, int j) {
+int get_piece_value(int i, int j, char colour) {
   int ans = 0;
   switch (board[i][j].name) {
-    case 'P': 
-      ans = 1;
-      break;
-    case 'N':
-      ans = 3;
-      break;
-    case 'B':
-      ans = 3;
-      break;
-    case 'R':
-      ans = 5;
-      break;
-    case 'Q':
-      ans = 9;
-      break;
-    case 'K':
-      ans = 60;
-      break;
+  case 'P':
+    ans = 1;
+    break;
+  case 'N':
+    ans = 3;
+    break;
+  case 'B':
+    ans = 3;
+    break;
+  case 'R':
+    ans = 5;
+    break;
+  case 'Q':
+    ans = 9;
+    break;
+  case 'K':
+    ans = 60;
+    break;
   }
 
-  if (board[i][j].colour != 'W') ans *= -1;
+  if (board[i][j].colour != colour)
+    ans *= -1;
   return ans;
 }
 
-
-int evaluate_board() {
-    int score = 0;
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            score += get_piece_value(i, j);
-        }
+int evaluate_board(char colour) {
+  int score = 0;
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      score += get_piece_value(i, j, colour);
     }
-    return score;
+  }
+  return score;
 }
 
 // Make a move struct
@@ -50,15 +50,16 @@ struct move {
   int y2;
 };
 
-
-void get_possible_moves(std::vector<move> &v) {
+void get_possible_moves(std::vector<move> &v, char colour) {
   for (int x1 = 0; x1 < 8; x1++) {
     for (int y1 = 0; y1 < 8; y1++) {
-      if (board[x1][y1].colour != 'W') continue; 
-      
+      if (board[x1][y1].colour != colour)
+        continue;
+
       for (int x2 = 0; x2 < 8; x2++) {
         for (int y2 = 0; y2 < 8; y2++) {
-          if (board[x2][y2].colour == 'W') continue;
+          if (board[x2][y2].colour == colour)
+            continue;
           piece temp1 = board[x1][y1];
           piece temp2 = board[x2][y2];
           if (movePiece(x1, y1, x2, y2)) {
@@ -72,26 +73,25 @@ void get_possible_moves(std::vector<move> &v) {
   }
 }
 
-// Need to not move when getting possible moves
-move decide_move() {
-    move best_move;
-    int best_score = -200;
-    std::vector<move> possible_moves;
-    get_possible_moves(possible_moves);
-  
-    for (move m : possible_moves) {
-      piece temp1 = board[m.x1][m.y1];
-      piece temp2 = board[m.x2][m.y2];
-      movePiece(m.x1, m.y1, m.x2, m.y2);
-      int score = evaluate_board();
-      
-      board[m.x1][m.y1] = temp1;
-      board[m.x2][m.y2] = temp2;
-      if (score > best_score) {
-          best_score = score;
-          best_move = m;
-          std::cout << "New best move: " << m.x1 << m.y1 << ' ' << m.x2 << m.y2;
-      }
+
+move decide_move(char colour) {
+  move best_move;
+  int best_score = -200;
+  std::vector<move> possible_moves;
+  get_possible_moves(possible_moves, colour);
+
+  for (move m : possible_moves) {
+    piece temp1 = board[m.x1][m.y1];
+    piece temp2 = board[m.x2][m.y2];
+    movePiece(m.x1, m.y1, m.x2, m.y2);
+    int score = evaluate_board(colour);
+
+    board[m.x1][m.y1] = temp1;
+    board[m.x2][m.y2] = temp2;
+    if (score > best_score) {
+      best_score = score;
+      best_move = m;
     }
-    return best_move;
+  }
+  return best_move;
 }

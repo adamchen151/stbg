45affd9cfd04821a644c0497cc534c3f891d7771
adamchen151 <105564290+adamchen151@users.noreply.github.com>
2023-08-04 16:26:14 -0400

    Update main.cpp

--- chess/main.cpp
+++ chess/main.cpp
@@ -1,16 +1,25 @@
 #include "ai.h"
 #include "functions.h"
+#include "move.h"
 #include <iostream>
 #include <vector>
-// TO-DO refactor the moves to 1 function
+
 int main() {
-  // std::cout << "\033[42m";
   int turn = 1;
   char colour;
   initializeBoard();
   std::pair<int, int> b, e; // beginning coords, end coords
+  
   bool isMove;
 
+  std::cout << "Do you want to play against an AI, or against another human (Y/N): ";
+  std::cin >> colour;
+  tolower(colour) == 'y' ? isAI = true : isAI = false;
+  if (isAI) {
+    std::cout << "What colour do you want the AI to be (W/B): ";
+    std::cin >> colour;
+    tolower(colour) == 'w' ? aiColour = 'W' : aiColour = 'B';
+  }
   do {
     colour = turn % 2 == 1 ? 'W' : 'B';
     colour == 'W' ? std::cout << "White's turn!" : std::cout << "Red's turn!";
@@ -18,7 +27,7 @@ int main() {
     turn++;
     isMove = false;
 
-    if (colour == 'B') {
+    if (!isAI || colour != aiColour) {
       while (!isMove) {
         do {
           b = getInput("\nInput the starting coordinates (x y): ");
@@ -39,11 +48,10 @@ int main() {
       }
     }
 
-    if (colour == 'W') {
-      move m = decide_move();
+    if (isAI && colour == aiColour) {
+      move m = decide_move(colour);
       movePiece(m.x1, m.y1, m.x2, m.y2);
     }
-    printBoard();
   } while (!isWin(colour));
 
   std::cout << "\nThank you for playing Penguin's Chess game!";

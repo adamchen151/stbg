273620ac922d3eb41ed759a46bbf414ad3c580cd
adamchen151 <105564290+adamchen151@users.noreply.github.com>
2023-08-04 16:26:50 -0400

    Update functions.h

--- chess/functions.h
+++ chess/functions.h
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <stdlib.h>
+#include <vector>
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 struct piece {
@@ -9,15 +9,10 @@ struct piece {
 };
 
 extern std::vector<std::vector<piece>> board;
+extern bool isAI;
+extern char aiColour;
 std::pair<int, int> getInput(std::string text);
 void initializeBoard();
 void printBoard();
 bool isWin(char colour);
-bool pawnMove(int x1, int y1, int x2, int y2);
-bool rookMove(int x1, int y1, int x2, int y2);
-bool bishopMove(int x1, int y1, int x2, int y2);
-bool knightMove(int x1, int y1, int x2, int y2);
-bool queenMove(int x1, int y1, int x2, int y2);
-bool kingMove(int x1, int y1, int x2, int y2);
-bool movePiece(int x1, int y1, int x2, int y2);
 #endif
